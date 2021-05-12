23f4a29af53c71217e91ae48cbea44be11292577
dwoojinkim <dwoojinkim@gmail.com>
2021-05-11 23:25:14 -0400

    Parallax BG
    Main Parallax BG is complete! (For now)

--- BasicSideScroller/Assets/Scripts/GameManagement/ParallaxBackground.cs
+++ BasicSideScroller/Assets/Scripts/GameManagement/ParallaxBackground.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿//Code taken from CodeMonkey on YouTube
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,19 +10,33 @@ public class ParallaxBackground : MonoBehaviour
 
     private Transform cameraTransform;
     private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
 
     // Start is called before the first frame update
     void Start()
     {
         cameraTransform = Camera.main.transform;
         lastCameraPosition = cameraTransform.position;
+
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width * transform.localScale.x / sprite.pixelsPerUnit;
+        Debug.Log("texture.width: " + texture.width);
+        Debug.Log("sprite.pixelsPerUnit: " + sprite.pixelsPerUnit);    
+        Debug.Log("textureUnitSizeX: " + textureUnitSizeX);
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        transform.position += deltaMovement;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
         lastCameraPosition = cameraTransform.position;
+
+        if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        {
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+        }
     }
 }
