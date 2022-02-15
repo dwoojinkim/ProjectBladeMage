f13e084e2270dace4cefec95d42705e48e998252
dwoojinkim <dwoojinkim@gmail.com>
2022-02-15 00:04:10 -0500

    Adding A* Project
    + Adding free version of A* Project
    
    https://arongranberg.com/astar/download

---
+++ BasicSideScroller/Assets/Scripts/Enemies/PathFinder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] private GameObject Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
