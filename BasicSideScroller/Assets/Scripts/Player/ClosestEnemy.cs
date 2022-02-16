ba6049bece1fbdfc5163ef06b14eea21229d992c
dwoojinkim <dwoojinkim@gmail.com>
2022-02-15 23:19:24 -0500

    Finding closest enemy
    + Started adding logic to find the closest enemy to the player.
    + Script named "ClosestEnemy" that is attached to the player that checks all enemies within a circle around the player.
    
    -> Still need to calculate distances from the player for each enemy captured in the circle

---
+++ BasicSideScroller/Assets/Scripts/Player/ClosestEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosestEnemy : MonoBehaviour
+{
+    private Collider2D[] enemies = new Collider2D[100];
+    private float circleRadius = 5.0f;
+    private LayerMask enemyLayer;
+    private ContactFilter2D enemyFilter;
+
+    private int numEnemies = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyLayer = LayerMask.GetMask("Enemy");
+        enemyFilter.SetLayerMask(enemyLayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        numEnemies = Physics2D.OverlapCircle((Vector2)this.transform.position, circleRadius, enemyFilter, enemies);
+
+        Debug.Log("Number of enemies in circle: " + numEnemies);
+    }
+}
