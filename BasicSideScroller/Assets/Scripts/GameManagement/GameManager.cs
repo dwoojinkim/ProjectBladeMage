896685f6870a60972d5012b0529a6c8b3dbda938
dwoojinkim <dwoojinkim@gmail.com>
2022-06-28 12:41:02 -0400

    Title Screen
    + Added title screen to work on menus a bit.
    + Importing TextMeshPro
    + Working on menu infrastructure for selecting menu items/buttons

--- BasicSideScroller/Assets/Scripts/GameManagement/GameManager.cs
+++ BasicSideScroller/Assets/Scripts/GameManagement/GameManager.cs
@@ -22,12 +22,19 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        /*if (Input.GetButtonDown("Up"))
+        {
+            //move up on menu
+        }
+        else if (Input.GetButtonDown("Down"))
+        {
+            //move down on menu
+        }*/
     }
 }
---
+++ BasicSideScroller/Assets/Scripts/Menu/TitleScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    public GameObject OptionSelector;
+    public Transform InitialMenu;
+
+    private List<GameObject> InitialMenuButtons = new List<GameObject>();
+
+
+    private int selectorPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitialMenuButtons = AddMenuButtons(InitialMenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private List<GameObject> AddMenuButtons(Transform menu)
+    {
+        List<GameObject> buttonList = new List<GameObject>();
+
+        for (int i = 0; i < menu.childCount; i++)
+            buttonList.Add(menu.GetChild(i).gameObject);
+
+        return buttonList;
+    }
+}
